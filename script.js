a39f5685fc03c07c25e814411edfaf306edf571f
ScarattiMatteo <159531852+ScarattiMatteo@users.noreply.github.com>
2024-02-16 12:17:57 +0100

    Test

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-"use str"
+"use strict";
+
+/* Map functions */
+let leafletMap = {
+    initialize: function() {
+                    var map = L.map('map', {zoomControl: false}).setView([0, 0], 13);
+                    
+                    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    {
+                        maxZoom: 19,
+                        attribution: '&copy; <a href = "https://www.openstreetmap.org/copyright">'
+                    }).addTo(map);
+
+                    L.control.zoom({
+                        position: 'bottomleft'
+                    }).addTo(map);
+                }
+
+    // Extra settings
+};
+
+Helpers.onload(function() {
+    console.log("Inizializing map...");
+    leafletMap.initialize();
+});
