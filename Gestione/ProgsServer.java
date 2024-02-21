09ebe70396595d0b9982eeb9d9b2aba1c8585eb0
AlexQuirozIV <quiroztanase.21388@iisvolta.it>
2024-02-21 13:49:51 +0100

    fine

--- Gestione/ProgsServer.java
+++ Gestione/ProgsServer.java
@@ -4,9 +4,10 @@ public class ProgGestione {
 
 
     public static void main(String[] args) {
-        Gestione[] places = new Gestione[2];
-        places[0] = new Gestione("Torre Zucchetti", "45.301689, 9.492247", "image1.jpg");
-        places[1] = new Gestione("IIS A. Volta", "45.303372, 9.498577", "image2.jpg");
+        GestorePlaces gestorePlaces = new GestorePlaces();
+
+        gestorePlaces.addPlace(new Gestione("Torre Zucchetti", "45.301689, 9.492247", "image1.jpg"));
+        gestorePlaces.addPlace(new Gestione("IIS A. Volta", "45.303372, 9.498577", "image2.jpg"));
 
 
         for (Gestione place : places) {
